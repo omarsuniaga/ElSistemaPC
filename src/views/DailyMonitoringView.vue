b64030ae894b09e03cebc6ce48ef9da2e92058a0
Omar Suniaga <omareduardo74@gmail.com>
2025-04-15 08:58:53 -0400

    Generando la PWA

--- src/views/DailyMonitoringView.vue
+++ src/views/DailyMonitoringView.vue
@@ -1,4 +1,4 @@
-<template>
+a<template>
   <div class="min-h-screen bg-gray-50 dark:bg-gray-900 py-6 px-4 sm:px-6 lg:px-8">
     <!-- Header con título y controles principales -->
     <header class="mb-8">
